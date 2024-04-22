f9416eadf07eba0b9069178af7e0a860ec273907
Arthur Cesar <arthurcesarmarcelino@gmail.com>
2024-04-22 20:13:25 -0300

    soma

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// ROTA
+    // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+        // GET - Pega Info
+        // POST - Cria uma info
+        // PUT - Altera toda a info
+        // PATH - Altera parte da info
+        // DELETE - Apaga uma info
+    
+    // Name - Um identificador da rota
+
+    // Function (Callback) - Responsavel por executar algum comando
+
+app.get('/soma', (req, res) => {
+    const soma = 100 + 1
+
+    res.send({soma: soma})
 })
 
 app.listen(3000);
